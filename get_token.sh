14cc4da7c22536d3c4f88fd5b0fdb360007da73c
sungryeol <sungryeolp@gmail.com>
2020-09-30 12:22:19 +0900

    :bug: bugfix

--- get_token.sh
+++ get_token.sh
@@ -2,5 +2,5 @@ CONTAINER=$(docker container ls | grep machine-learning | awk '{print $1}')
 echo $CONTAINER
 
 REPLACE="localhost"
-URL=$(docker container exec -it $CONTAINER jupyter notebook list | awk 'NR==2{print $1}' | sed "s/.0.0.0.0/${REPLACE}/")
+URL=$(docker container exec -it $CONTAINER jupyter notebook list | awk 'NR==2{print $1}' | sed "s/0\.0\.0\.0/${REPLACE}/")
 echo $URL
